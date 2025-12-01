1193b6cab55f94de54daef6114ee7074a4f5f9a1
John Dunning <fw@johndunning.com>
2025-11-30 23:07:35 -0800

    Add endorsements-gallery.json and render the list of people
    Add all the endorsement gallery photos.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,31 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from 'astro/loaders';
+import { glob } from "astro/loaders";
 
 export const collections = {
-    endorsements: defineCollection({
-        loader: glob({ pattern: "**/*.json", base: "./src/content/endorsements" }),
-        schema: z.array(z.object({
-            category: z.string(),
-            endorsements: z.array(
-                z.object({
-                    name: z.string(),
-                    title: z.string()
-                })
-            )
-        }))
-    })
+	endorsements: defineCollection({
+		loader: glob({
+			pattern: "endorsements.json",
+			base: "src/content/endorsements"
+		}),
+		schema: z.array(z.object({
+			category: z.string(),
+			endorsements: z.array(
+				z.object({
+					name: z.string(),
+					title: z.string()
+				})
+			)
+		}))
+	}),
+	endorsementsGallery: defineCollection({
+		loader: glob({
+			pattern: "endorsements-gallery.json",
+			base: "src/content/endorsements"
+		}),
+		schema: ({ image }) => z.array(z.object({
+			name: z.string(),
+			title: z.string(),
+			photo: image()
+		}))
+	})
 };
